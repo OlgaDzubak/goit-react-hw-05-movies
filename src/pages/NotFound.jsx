3a01e9e4f42a8a4e0f5db052a3ecb3e3394798c6
Olga Dzubak <118764742+OlgaDzubak@users.noreply.github.com>
2023-07-07 12:47:54 +0300

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,1 +1,6 @@
-export const NotFound = () => { return <p>Page not found! Please chech the path and try again.</p> }
+export const NotFound = () => { 
+  return (
+  <section>
+      <p>Page not found! Please chech the path and try again.</p>
+  </section>)
+}
